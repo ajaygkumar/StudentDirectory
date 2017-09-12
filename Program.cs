da38660c9ffb352f3ca1e0abb1084f6921186ea0
ajaygkumar <31571077+ajaygkumar@users.noreply.github.com>
2017-09-12 01:17:18 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter W(To write the student details) or R(To read the student details)");
+            String a;
+            a = Convert.ToString(Console.ReadLine());
+            String W = "W";
+            //W = Convert.ToString(Console.ReadLine());
+            if (a == W)
+            {
+                student ajay = new student();
+                ajay.setstudent();
+                ajay.getstudent();
+            }
+            else
+            {
+                staticstudent s = new staticstudent();
+                s.getstaticstudent();
+            }
+        }
+        public void getstaticstudent()
+        {
+            Console.WriteLine("Student details are: {0}, {1}, {2}, {3}", 1, "Ajay", "Kumar", "ajaykumar@gmail.com");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudentDirectory
+{
+    class student
+    {
+
+        int id;
+        string firstname;
+        string lastname;
+        string emailid;
+        public void setstudent()
+        { 
+            Console.WriteLine("Enter the Student ID:");
+            id = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the First Name:");
+            firstname = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter the Last Name:");
+            lastname = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Enter the Email ID:");
+            emailid = Convert.ToString(Console.ReadLine());
+        }
+        public void getstudent()
+        {
+            Console.WriteLine("Student details are: {0}, {1}, {2}, {3}", id, firstname, lastname, emailid);
+            Console.ReadKey();
+        }
+    }
+}
